10cc9a852e7f3947f689a4f3c109316f23266088
nemo <nemo724@naver.com>
2024-09-16 22:32:21 +0900

    1의 자리수부터 곱할 수 있도록 나누는 변수 추

--- 백준 2588번/곱셈.c
+++ 백준 2588번/곱셈.c
@@ -1,11 +1,20 @@
 ﻿#include <stdio.h>
-
+#include <math.h>
 int main(void)
 {int x=0;
 int y = 0;
 
 scanf_s("%d%d", &x, &y);
 
+for (int i = 1; i <=3; i++)
+{int div = pow(10, i);
+
+
+
+
+
+
+}
 
 
 
