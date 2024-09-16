1fdfbbe3b2fcccba290748ff2833c0b72d2daa3f
nemo <nemo724@naver.com>
2024-09-16 23:32:48 +0900

    10 제곱계산 위한 배열 변수 추가 및 y값 초기화

--- 백준 2588번/곱셈.c
+++ 백준 2588번/곱셈.c
@@ -2,24 +2,19 @@
 #include <math.h>
 int main(void)
 {int x=0;
-int y = 0;
-
+int y=0;
+int sum=0;
+int print_y[3];
 scanf_s("%d%d", &x, &y);
 
-for (int i = 1; i <=3; i++)
-{int div = pow(10, i);
+for (int i = 2; i >= 0; i--)
+{int div = (int)pow(10, i);
+print_y[i] = y / div;
+y = y % div;}
 
+for (int i = 0; i < 3; i++)
+{printf("%d\n", print_y[i] * x);
+sum = sum + (print_y[i] * x * (int)pow(10, i));}
 
-
-
-
-
-}
-
-
-
-
-
-
-
+printf("%d", sum);
 }
